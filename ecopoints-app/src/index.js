d1b3ec8386489c9efdc12ed79a7c60dbbcbfe68b
kill1011 <libronangelo406@gmail.com>
2025-04-10 21:34:55 +0800

    initial commit

--- ecopoints-app/src/index.js
+++ ecopoints-app/src/index.js
@@ -1,9 +1,6 @@
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
