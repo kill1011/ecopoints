deae11e2a225f5e044f5077cb81dfb0a594acd23
kill1011 <libronangelo406@gmail.com>
2025-04-10 22:25:57 +0800

    Update index.js

--- ecopoints-backend/index.js
+++ ecopoints-backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://ecopoints-teal.vercel.app' // Update with your actual frontend URL if deployed
+    'https://ecopoints-teal.vercel.app/' // Update with your actual frontend URL if deployed
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
